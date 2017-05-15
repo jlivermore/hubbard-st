b9ebaf45bb66327601b0aaa744998c22a5759905
jennlivermore <jennblivermore@gmail.com>
2017-05-15 00:50:41 -0400

    png

--- home.js
+++ home.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
 			counter = index + 1;
 			links = index + 1;
 			$('.map img').attr('src','images/'+counter+'.png');
-			$('.google img').attr('src','images/streetview/'+counter+'.jpg');
+			$('.google img').attr('src','images/streetview/'+counter+'.png');
 			$('.letter img').attr('src','images/letter/'+counter+'.png');
 		});
 	});
